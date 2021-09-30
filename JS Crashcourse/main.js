c4834376dcc735a152c9982012970d652a4c1a7f
Hawk Yang <hawk.y.f.yang@outlook.com>
2021-09-29 20:36:03 -0400

    Update main.js

--- JS Crashcourse/main.js
+++ JS Crashcourse/main.js
@@ -1,2 +1,31 @@
 alert("Hello");
 console.log("Yuh yeet");
+console.error("example error");
+console.warn("example warning");
+
+
+let age =30;
+console.log("your age is", age);
+age = 16
+console.log("your age is now", age);
+
+const age2 = 32
+console.log("your age2 is", age2);
+
+
+const name = "John";
+age = 30;
+const rating = 3.7;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z;
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
